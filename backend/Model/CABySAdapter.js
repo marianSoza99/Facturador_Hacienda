98d40a11ea5fdc780f4cc99b2f307a0d390d493b
josue0702 <48897497+josue0702@users.noreply.github.com>
2021-03-26 18:20:27 -0600

    Filtros de Categoria 1 y 2 del cabys

--- backend/Model/CABySAdapter.js
+++ backend/Model/CABySAdapter.js
@@ -1,19 +1,33 @@
 const xlsx = require('xlsx');
 
-var workbook = xlsx.readFile("../src/Catalogo-de-bienes-servicios.xlsx", {raw:true});
-var sheet = workbook.Sheets["Cabys"];
-var cabys = xlsx.utils.sheet_to_json(sheet);
+var workbook;
+var Sheets;
+var cabys;
+var first = true;
 
-var categories = [];
-
-cabys.forEach(function(record){
-    if(!categories.includes(record.__EMPTY_1)){
-        categories.push(record.__EMPTY_1);
+var getCategory1 = function(res){
+    if (first){
+        workbook = xlsx.readFile("./Model/src/Catalogo-de-bienes-servicios.xlsx");
+        sheet = workbook.Sheets["Cabys"];
+        cabys = xlsx.utils.sheet_to_json(sheet);
+        first = false;
     }
-});
+    cabys.forEach(function(record){
+        if(!res.includes(record.__EMPTY_1)){
+            res.push(record.__EMPTY_1);
+        }
+    });
+    res.shift();
+};
 
-var categorie1 = Object.fromEntries(Object.entries(cabys).filter(([key, value]) => {
-    value.__EMPTY_1 == categories[1];
-}));
+var getCategory2 = function(filter, res){
+    var filteredRecords = Object.entries(cabys).filter(([key, value]) => value.__EMPTY_1 == filter);
 
-console.log(categorie1);
+    filteredRecords.forEach(function(record){
+        if (!res.includes(record[1].__EMPTY_3)){
+            res.push(record[1].__EMPTY_3);
+        }
+    });
+};
+
+module.exports = { getCategory1, getCategory2 };
--- backend/routes/BillRouter.js
+++ backend/routes/BillRouter.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
-const adapter = require('../Adapter');
+const adapter = require('../Model/Adapter');
+const cabysAdapter = require('../Model/CABySAdapter');
+
+var temp;
 
 router.route('/new').get((req, res) => {
-    console.log('New Bill');
+    var resp = [];
+    cabysAdapter.getCategory1(resp);
+    temp = resp;
+    res.send(resp);
+});
+
+router.route('/test').get((req, res) => {
+    var resp = [];
+    cabysAdapter.getCategory2(temp[0], resp);
+    res.send(resp);
 });
 
 module.exports = router;
