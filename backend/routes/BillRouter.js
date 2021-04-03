4a03ab5addd15fec79ae83d6c832aadeb5180375
Dernudo <43736095+Dernudo@users.noreply.github.com>
2021-04-03 17:30:32 -0600

    Adaptador de CaByS listo

--- backend/routes/BillRouter.js
+++ backend/routes/BillRouter.js
@@ -4,47 +4,56 @@ const cabysAdapter = require('../Model/CABySAdapter');
 router.route('/cats').get((req, res) => {
     var resp = []
     cabysAdapter.getCategory1(resp);
-    cat1 = resp;
-    res.send(resp);
+    cat1 = resp[0].categories;
+    res.send(resp[0]);
 });
 
 router.route('/cat1').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory2(cat1[0].codigo, resp);
+    cabysAdapter.getCategory2(cat1[1].codigo, resp);
     cat2 = resp[0].categories;
-    pro1 = resp[0].products;
     res.send(resp[0]);
 });
 
 router.route('/cat2').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory3(cat2[0].codigo, pro1, resp);
+    cabysAdapter.getCategory3(cat2[0].codigo, resp);
     cat3 = resp[0].categories;
-    pro2 = resp[0].products;
     res.send(resp[0]);
 });
 
 router.route('/cat3').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory4(cat3[0].codigo, pro2, resp);
+    cabysAdapter.getCategory4(cat3[0].codigo, resp);
     cat4 = resp[0].categories;
-    pro3 = resp[0].products;
     res.send(resp[0]);
 });
 
 router.route('/cat4').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory5(cat4[0].codigo, pro3, resp);
+    cabysAdapter.getCategory5(cat4[0].codigo, resp);
     cat5 = resp[0].categories;
-    pro4 = resp[0].products;
     res.send(resp[0]);
 });
 
 router.route('/cat5').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory6(cat5[0].codigo, pro4, resp);
+    cabysAdapter.getCategory6(cat5[0].codigo, resp);
     cat6 = resp[0].categories;
-    pro5 = resp[0].products;
+    res.send(resp[0]);
+});
+
+router.route('/cat6').get((req, res) => {
+    var resp = [];
+    cabysAdapter.getCategory7(cat6[0].codigo, resp);
+    cat7 = resp[0].categories;
+    res.send(resp[0]);
+});
+
+router.route('/cat7').get((req, res) => {
+    var resp = [];
+    cabysAdapter.getCategory8(cat7[0].codigo, resp);
+    cat8 = resp[0].categories;
     res.send(resp[0]);
 });
 
