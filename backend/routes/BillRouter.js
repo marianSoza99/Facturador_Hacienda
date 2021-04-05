250e4a45cd83cbf616efecb4d072c7ddddf9658e
Dernudo <43736095+Dernudo@users.noreply.github.com>
2021-04-05 16:15:25 -0600

    Update BillRouter.js

--- backend/routes/BillRouter.js
+++ backend/routes/BillRouter.js
@@ -8,52 +8,48 @@ router.route('/cats').get((req, res) => {
     res.send(resp[0]);
 });
 
+
+// { codigo: "" }
+
 router.route('/cat1').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory2(cat1[1].codigo, resp);
-    cat2 = resp[0].categories;
+    cabysAdapter.getCategory2(req.codigo, resp);
     res.send(resp[0]);
 });
 
 router.route('/cat2').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory3(cat2[0].codigo, resp);
-    cat3 = resp[0].categories;
+    cabysAdapter.getCategory3(req.codigo, resp);
     res.send(resp[0]);
 });
 
 router.route('/cat3').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory4(cat3[0].codigo, resp);
-    cat4 = resp[0].categories;
+    cabysAdapter.getCategory4(req.codigo, resp);
     res.send(resp[0]);
 });
 
 router.route('/cat4').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory5(cat4[0].codigo, resp);
-    cat5 = resp[0].categories;
+    cabysAdapter.getCategory5(req.codigo, resp);
     res.send(resp[0]);
 });
 
 router.route('/cat5').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory6(cat5[0].codigo, resp);
-    cat6 = resp[0].categories;
+    cabysAdapter.getCategory6(req.codigo, resp);
     res.send(resp[0]);
 });
 
 router.route('/cat6').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory7(cat6[0].codigo, resp);
-    cat7 = resp[0].categories;
+    cabysAdapter.getCategory7(req.codigo, resp);
     res.send(resp[0]);
 });
 
 router.route('/cat7').get((req, res) => {
     var resp = [];
-    cabysAdapter.getCategory8(cat7[0].codigo, resp);
-    cat8 = resp[0].categories;
+    cabysAdapter.getCategory8(req.codigo, resp);
     res.send(resp[0]);
 });
 
