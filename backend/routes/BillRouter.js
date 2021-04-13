33c8143d99875d6d2c573adea8918c52d1ee10bc
Dernudo <43736095+Dernudo@users.noreply.github.com>
2021-04-13 02:59:24 -0600

    CaByS Parametrizado

--- backend/routes/BillRouter.js
+++ backend/routes/BillRouter.js
@@ -5,54 +5,26 @@ import express from 'express'
 const router = express.Router();
 import { getCategory1, getCategory2, getCategory3, getCategory4, getCategory5, getCategory6, getCategory7, getCategory8 } from '../Model/CABySAdapter.js'
 
-router.route('/cats').get((req, res) => {
+router.route('/cat/:category/:code').get((req, res) => {
     var resp = []
-    getCategory1(resp);
-    res.send(resp[0]);
-});
-
-
-// { codigo: "" }
-
-router.route('/cat1/:code').get((req, res) => {
-    var resp = [];
-    getCategory2(req.params.code, resp);
-    res.send(resp[0]);
-});
-
-router.route('/cat2').get((req, res) => {
-    var resp = [];
-    getCategory3(req.codigo, resp);
-    res.send(resp[0]);
-});
-
-router.route('/cat3').get((req, res) => {
-    var resp = [];
-    getCategory4(req.codigo, resp);
-    res.send(resp[0]);
-});
-
-router.route('/cat4').get((req, res) => {
-    var resp = [];
-    getCategory5(req.codigo, resp);
-    res.send(resp[0]);
-});
-
-router.route('/cat5').get((req, res) => {
-    var resp = [];
-    getCategory6(req.codigo, resp);
-    res.send(resp[0]);
-});
-
-router.route('/cat6').get((req, res) => {
-    var resp = [];
-    getCategory7(req.codigo, resp);
-    res.send(resp[0]);
-});
-
-router.route('/cat7').get((req, res) => {
-    var resp = [];
-    getCategory8(req.codigo, resp);
+    switch(req.params.category){
+        case '1':
+            getCategory1(resp);
+        case '2':
+            getCategory2(req.params.code,resp);
+        case '3':
+            getCategory3(req.params.code,resp);
+        case '4':
+            getCategory4(req.params.code,resp);
+        case '5':
+            getCategory5(req.params.code,resp);
+        case '6':
+            getCategory6(req.params.code,resp);
+        case '7':
+            getCategory7(req.params.code,resp);
+        case '8':
+            getCategory8(req.params.code,resp);
+    }
     res.send(resp[0]);
 });
 
