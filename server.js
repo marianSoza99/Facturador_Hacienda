7c488a716f5f1ed014c02621eef3a7658c25d0e8
Dernudo <43736095+Dernudo@users.noreply.github.com>
2021-04-18 23:58:37 -0600

    Cambios

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import 'dotenv/config.js'
 import path from 'path'
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 process.env.__dirname = (() => {let x = path.dirname(decodeURI(new URL(import.meta.url).pathname)); return path.resolve( (process.platform == "win32") ? x.substr(1) : x ); })();
 
 app.use(cors());
